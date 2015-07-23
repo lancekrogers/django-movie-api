612b32b081f046dd5cc90b33baad3a9587ee6e35
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-23 15:27:05 -0400

    finsished the function based api views

--- crud/api/urls.py
+++ crud/api/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import include, url
-from .views import my_get_api
+from .views import my_get_api, post_api, delete_api, put_api
 
 urlpatterns = [
 
-    url(r'^movie_list/$', my_get_api, name='get_api')
+    url(r'^get/', my_get_api, name='get_api'),
+    url(r'^post/', post_api),
+    url(r'^delete/', delete_api),
+    url(r'^put/', put_api)
+
 
 ]
--- crud/api/views.py
+++ crud/api/views.py
@@ -1,33 +1,38 @@
 #from django.contrib.sites import requests
 from django.core import serializers
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 import json
 from movie.models import Movie
 # Create your views here.
 
 
-# Get request view
-
-
-
+@csrf_exempt
 def my_get_api(request):
    qs = Movie.objects.all()
    return HttpResponse(serializers.serialize('json', qs), content_type='application/json')
 
+@csrf_exempt
+def post_api(request, *args, **kwargs):
+    movie = Movie.objects.create(title=request.POST['title'])
+    movie.save()
+    return HttpResponse(status=201)
 
-
-# Post request view
-
-
-
-
-# Delete request view
-
+@csrf_exempt
+def delete_api(request, *args, **kwargs):
+   movie = Movie.objects.filter(pk=request.POST['pk'])
+   movie.delete()
+   return HttpResponse(status=200)
 
 
 
 # Update (put or patch) request view
-
+@csrf_exempt
+def put_api(request):
+    first_movie = Movie.objects.filter(pk=request.POST['pk'])
+    first_movie.delete()
+    new_movie = Movie.objects.create(pk=request.POST['pk'], title=request.POST['title'])
+    return HttpResponse(status=200)
 
 
